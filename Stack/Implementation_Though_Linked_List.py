abcf16619ed337f851475d6bec8a01d5d8347b57
Abhay Narvade <abhaynaravday123@gmail.com>
2025-12-23 20:36:26 +0530

    Added Implemention of stack using LinkedList

--- Stack/Implementation_Though_Linked_List.py
+++ Stack/Implementation_Though_Linked_List.py
@@ -1,1 +1,65 @@
 # Implementation Thorough Linked List 
+
+class Node :
+    def __init__(self,data = None):
+        self.data  = data 
+        self.Next = None
+
+class stackLinkedList:
+    def __init__(self):
+        self.top = None
+        self.length = 0
+    
+    # is empty method 
+
+    def isEmpty(self):
+        return self.length == 0
+    
+    def push (self,item):
+        newNode = Node(item)
+        newNode.Next = self.top
+        self.top = newNode
+        self.length += 1
+    
+    def pop(self):
+        if self.length == 0 :
+            print('Stack Is Empty')
+            return 
+        returnData = self.top.data
+        self.top = self.top.Next
+        self.length -=1
+        return returnData
+            
+
+    def peek(self):
+        if self.top is not None :
+            return self.top.data 
+        print('Stack is empty ')
+
+
+
+    def printStackData(self):
+        current = self.top 
+        while current is not None :
+            print(f'[{current.data}]',end='->')
+            current = current.Next
+        print('\n')
+
+s =  stackLinkedList()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(40)
+s.push(50)
+
+
+
+s.printStackData()
+
+
+s.pop()
+s.pop()
+s.pop()
+
+
+s.printStackData()
